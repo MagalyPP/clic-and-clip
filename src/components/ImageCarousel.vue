069eb75639cff2d645e330a837e7756ab34b8aa6
KukoCL <webphantom.hc@gmail.com>
2026-01-05 19:12:35 -0300

    spinning circle

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -103,11 +103,6 @@ watch(currentImage, () => {
   transform: translate(-50%, -50%);
   color: #972805;
   z-index: 10;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
 }
 
 .carousel-image {
